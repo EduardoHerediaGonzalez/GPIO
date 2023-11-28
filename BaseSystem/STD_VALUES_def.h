cdcc97007272e16f73d813c6e9188ca2bfbc0591
Eduardo Heredia Gonzalez <EduardoHerediaGonzalez@gmail.com>
2023-11-28 16:44:23 -0600

    Added PLATFORM_TYPES_def.h and STD_VALUES_def.h to the project.

---
+++ BaseSystem/STD_VALUES_def.h
@@ -0,0 +1,28 @@
+/**
+ * Copyright © 2023 
+ *
+ * @file    STD_VALUES_def.h
+ * @note
+ *        ______________      
+ *        eduardohg	          
+ *        Diseñador           
+ *
+ * @brief   STD_VALUES_def 
+ * @version No oficial
+ *     
+ */
+
+#ifndef STD_VALUES_def_H_
+#define STD_VALUES_def_H_
+
+/* system headers */
+
+/* exported macros */
+#define STD_VALUES_ZERO	(0U)
+#define STD_VALUES_ONE	(1U)
+#define STD_VALUES_OFF	(0U)
+#define STD_VALUES_ON	(1U)
+
+/* exported types */
+
+#endif /* STD_VALUES_def_H_ */
